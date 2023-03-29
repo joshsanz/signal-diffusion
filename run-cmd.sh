4e0e04f9645584f48b298df9ea52e77c281642ae
Josh Sanz <jsanz@berkeley.edu>
2023-03-28 21:49:16 -0700

    Record current run command

---
+++ run-cmd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export MODEL_NAME='OFA-Sys/small-stable-diffusion-v0'
+export $dataset_name='lambdalabs/pokemon-blip-captions'
+
+accelerate launch --mixed_precision=fp16 train_text_to_image.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --dataset_name=$dataset_name \
+  --resolution=512 --center_crop --random_flip \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --gradient_checkpointing \
+  --max_train_steps=50000 \
+  --learning_rate=2e-5 \
+  --max_grad_norm=1 \
+  --lr_scheduler="constant" --lr_warmup_steps=0 \
+  --output_dir="sd-pokemon-model.2" \
+  --enable_xformers_memory_efficient_attention \
+  --validation_prompt="a cute blue dragon"
+
