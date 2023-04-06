38339e327f93e5f9447d9824faa82a5083271f13
Josh Sanz <jsanz@berkeley.edu>
2023-04-06 10:44:51 -0700

    Add direct base-fine_tune comparisons

--- infer_text_to_image.py
+++ infer_text_to_image.py
@@ -22,6 +22,7 @@ steps = 30
 
 parser = argparse.ArgumentParser("An inference pipeline for finetuned SDv1.5")
 parser.add_argument("--path", default=model_path, help="Path to fine-tuned weights")
+parser.add_argument("--comparison", default=None, help="Path to original weights for comparison")
 parser.add_argument("-p", "--prompt", default=prompt, help="Text prompt for generation")
 parser.add_argument("-n", "--num-images", type=int, default=1, help="Number of images to generate from prompt")
 parser.add_argument("-o", "--output", default="out.png", help="Output filename")
@@ -41,9 +42,20 @@ images = [pipe(prompts[i], num_inference_steps=args.steps, guidance_scale=7.5,).
           for i in range(args.num_images)]
 images = [np.asarray(im) for im in images]
 
+# Generate comparison images
+if args.comparison is not None:
+    torch.manual_seed(args.seed)
+    pipe = StableDiffusionPipeline.from_pretrained(os.path.expanduser(args.comparison), torch_dtype=torch.float16)
+    pipe.to("cuda")
+    comparisons = [pipe(prompts[i], num_inference_steps=args.steps, guidance_scale=7.5,).images[0]
+                   for i in range(args.num_images)]
+    comparisons = [np.asarray(comp) for comp in comparisons]
+    images.extend(comparisons)
+
 # Plot
-nrow = int(np.floor(np.sqrt(args.num_images)))
-ncol = int(np.ceil(args.num_images / nrow))
+ncol = int(np.ceil(np.sqrt(args.num_images))) if args.comparison is None else args.num_images
+nrow = int(np.ceil(args.num_images / ncol))
+
 images.extend([np.zeros_like(images[0]) for _ in range(nrow * ncol - args.num_images)])
 
 grid = image_grid(np.array(images), ncol)
@@ -51,4 +63,3 @@ fig = plt.figure(figsize=(nrow * 3, ncol * 3))
 plt.imshow(grid)
 plt.axis('off')
 plt.savefig(args.output, bbox_inches='tight')
-
