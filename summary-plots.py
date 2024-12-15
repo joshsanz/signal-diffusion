d12510f918bb9e9af57cf2ce2e063741348f2a7d
Josh Sanz <jsanz@berkeley.edu>
2024-12-14 22:05:18 -0800

    plot updates

--- summary-plots.py
+++ summary-plots.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
-plt.rcParams['font.size'] = 14
+plt.rcParams['font.size'] = 15
 plt.rcParams['font.family'] = 'sans-serif'
 plt.rcParams['font.sans-serif'] = ['Tahoma', 'DejaVu Sans',
                                    'Lucida Grande', 'Verdana']
 
 # Load the data
 classifier_acc_file = 'eeg_classification/bestmodels/accuracy.csv'
-gen_metrics_sd_file = 'metrics/metrics-sd.json'
+gen_metrics_sd_file = 'metrics/metrics-sdv1.json'
 gen_metrics_dis_file = 'metrics/metrics-dis.json'
 gen_metrics_self_file = 'metrics/metrics-self.json'
 gen_metrics_sdxl_file = 'metrics/metrics-sdxl-vae.json'
@@ -29,16 +29,16 @@ classifier_acc.rename_axis("Train", axis=0, inplace=True)
 print(classifier_acc)
 print(classifier_acc.values)
 
-# gen_metrics = pd.DataFrame()
-# for mf in metrics_files:
-#     df = pd.read_json(mf, orient='index')
-#     df["Generator"] = ["-".join(os.path.basename(mf).split("-")[1:]).split(".")[0]] * 2
-#     gen_metrics = pd.concat([gen_metrics, df])
-# gen_metrics = gen_metrics.reset_index(names="Featurizer")
-# gen_metrics = gen_metrics.replace("dinov2-vit-l-14", "DINOv2")
-# gen_metrics = gen_metrics.replace("clip-vit-l-14", "CLIP")
-# gen_metrics.set_index(["Generator", "Featurizer"], inplace=True)
-# print(gen_metrics)
+gen_metrics = pd.DataFrame()
+for mf in metrics_files:
+   df = pd.read_json(mf, orient='index')
+   df["Generator"] = ["-".join(os.path.basename(mf).split("-")[1:]).split(".")[0]] * 2
+   gen_metrics = pd.concat([gen_metrics, df])
+gen_metrics = gen_metrics.reset_index(names="Featurizer")
+gen_metrics = gen_metrics.replace("dinov2-vit-l-14", "DINOv2")
+gen_metrics = gen_metrics.replace("clip-vit-l-14", "CLIP")
+gen_metrics.set_index(["Generator", "Featurizer"], inplace=True)
+print(gen_metrics)
 
 # Plot the data
 # Classifier accuracy
@@ -62,8 +62,8 @@ x = np.arange(3)  # the label locations
 width = 0.15  # the width of the bars
 multiplier = 0
 
-testsets = ['real_eeg', 'dis', 'sd']
-trainsets = ['real_eeg', 'real_eeg EMA', 'dis', 'dis EMA', 'sd', 'sd EMA']
+testsets = ['real_eeg', 'dis', 'sdv1']
+trainsets = ['real_eeg', 'real_eeg EMA', 'dis', 'dis EMA', 'sdv1', 'sdv1 EMA']
 colors = ['C0', 'C0', 'C1', 'C1', 'C2', 'C2']
 alphas = [1, 0.5, 1, 0.5, 1, 0.5]
 for trainset, c, a in zip(trainsets, colors, alphas):
@@ -89,13 +89,13 @@ plt.close()
 
 # Generative Metrics
 # KDD
-# fig, ax = plt.subplots(figsize=(8, 5))
-# gen_metrics.unstack().plot.bar(y="kernel_inception_distance_mean", ax=ax,
-#                                yerr="kernel_inception_distance_std", capsize=5)
-# plt.xticks(rotation=0)
-# plt.ylabel(r"MMD$\downarrow$")
+fig, ax = plt.subplots(figsize=(8, 5))
+gen_metrics.unstack().plot.bar(y="kernel_inception_distance_mean", ax=ax,
+                              yerr="kernel_inception_distance_std", capsize=5)
+plt.xticks(rotation=0)
+plt.ylabel(r"MMD$\downarrow$")
 # plt.title("Maximal Mean Discrepancy (MMD)")
-# plt.tight_layout()
-# # plt.show()
-# plt.savefig('kdd-bar.png')
-# plt.close()
+plt.tight_layout()
+# plt.show()
+plt.savefig('kdd-bar.png')
+plt.close()
